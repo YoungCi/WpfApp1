5f43970deab043b5c7f13a47c4ce8e705e6e4d56
叶加博 <mizukiluke@outlook.com>
2018-06-03 16:07:49 +0800

    re password

--- WpfApp1/Manage_Emp.xaml.cs
+++ WpfApp1/Manage_Emp.xaml.cs
@@ -29,17 +29,14 @@ namespace WpfApp1
             InitializeComponent();
             this.frame = frame;
             Emp_DataGrid.ItemsSource = list;
-            list.Add(new EmpObj("C00001", "叶加博", Sex.男, 22, -100));
-            list.Add(new EmpObj("C00002", "杨思旖", Sex.女, 21, 100));
+            list.Add(new EmpObj("C00001", "叶加博", Sex.男, 22, -100,UserType.员工));
+            list.Add(new EmpObj("C00002", "杨思旖", Sex.女, 21, 100,UserType.管理员));
            
         }
         private void EditAction(object sender, RoutedEventArgs e)
         {
             var index = Emp_DataGrid.SelectedIndex;
             DataGridRow rowContainer = (DataGridRow)Emp_DataGrid.ItemContainerGenerator.ContainerFromIndex(index);
-
-            var box = Emp_DataGrid.Columns[1].GetCellContent(rowContainer) as CheckBox;
-            box.IsChecked = !box.IsChecked;
             Console.WriteLine(list[index]);
             MyDialog dialog = new MyDialog(list[index],false);
             dialog.ShowDialog();
@@ -49,9 +46,22 @@ namespace WpfApp1
                 list[index] = dialog.getObjResult() as EmpObj;
             }
         }
+        private void resetPasswordAction(object sender, RoutedEventArgs e)
+        {
+            var index = Emp_DataGrid.SelectedIndex;
+            String idKey = list[index].工号;
+            RePasswordDialog rep = new RePasswordDialog();
+            rep.ShowDialog();
+            if(rep.DialogResult==true)
+            {
+                String hashPassword = rep.GetHashPassword();
+                //插入
+            }
+        }
         private void AddEmp(object sender, RoutedEventArgs e)
         {
-
+            MyDialog dialog = new MyDialog(typeof(EmpObj));
+            dialog.ShowDialog();
         }
         private void OnDataGridAutoGeneratingColumn(
             object sender, DataGridAutoGeneratingColumnEventArgs e)
---
+++ WpfApp1/RePasswordDialog.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// RePasswordDialog.xaml 的交互逻辑
+    /// </summary>
+    public partial class RePasswordDialog : Window
+    {
+        public RePasswordDialog()
+        {
+            InitializeComponent();
+        }
+        public String GetHashPassword()
+        {
+            return md5helper.encrypt(rePasswordBox.Password);
+        }
+        private void Submit(object sender, RoutedEventArgs e)
+        {
+
+            DialogResult = true;
+        }
+
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
