459e53f638ca416d719e58f574dd8b8527ec4959
805121644 <805121644@qq.com>
2018-06-04 23:39:06 +0800

    暗暗

---
+++ WpfApp1/AddCustomer.xaml.cs
@@ -0,0 +1,77 @@
+﻿using DataGridDemo;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// AddCustomer.xaml 的交互逻辑
+    /// </summary>
+    public partial class AddCustomer : Page
+    {
+        Sql helper = new Sql();
+        Frame frame;
+        ObservableCollection<CustObj> list = new ObservableCollection<CustObj>();
+        public AddCustomer(Frame frame)
+        {
+            InitializeComponent();
+            this.frame = frame;
+        }
+        private void OnDataGridAutoGeneratingColumn(
+           object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            Console.WriteLine(e.PropertyName);
+            if (e.PropertyName == "Check")
+            {
+                e.Cancel = true;
+            }
+
+        }
+        private void add_cust(object sender, RoutedEventArgs e)
+        {
+            MyDialog dialog = new MyDialog(typeof(CustObj), "添加客户");
+            dialog.ShowDialog();
+            ObservableCollection<CustObj> newList = dialog.getObjectList();
+            foreach (var item in newList)
+            {
+                int operatorCode = helper.Cust_add(item.客户号.Trim(), item.姓名.Trim(), item.电话.Trim(), item.地址.Trim());
+                if (operatorCode == 0)
+                {
+                    resetList();
+                    return;
+                }
+            }
+        }
+        private void resetList(DataRow[] rows)
+        {
+            list.Clear();
+            foreach (var r in rows)
+            {
+                list.Add(new CustObj(r["cus_no"].ToString().Trim(),
+                                     r["cus_name"].ToString().Trim(),
+                                      r["tel"].ToString().Trim(),
+                                       r["addr"].ToString().Trim()));
+            }
+        }
+        private void resetList()
+        {
+            var rows = helper.find_all("customer");
+            resetList(rows);
+        }
+
+    }
+}
